9b9925299988d4c9e7b57cf2afb50f9f802211f5
Henry <nyanlinaung3722@gmail.com>
2025-03-08 03:59:03 +0800

    changed express config name and moved under one config folder

--- Express/routes/adminRoutes.js
+++ Express/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const supabase = require("../config/supabaseClient"); // Centralized Supabase client
+const supabase = require("../../src/config/supabaseAdminClient"); // Centralized Supabase client
 
 // Endpoint to reset a user's password using their userId (unchanged)
 router.post("/user/:userId/reset-password", async (req, res) => {
