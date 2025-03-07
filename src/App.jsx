5f0fa5d9f1296318625ffcb49b2bd154c53b5176
JarvisSng <150510665+JarvisSng@users.noreply.github.com>
2025-03-08 01:43:05 +0800

    Optimize folders and files

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import "./index.css"; // Global styles
 import AdminLogin from "./pages/adminLogin.jsx";
 import Detector from "./pages/Detector.jsx";
 import LandingPage from "./pages/landingPage.jsx";
-import LoginEmail from "./pages/loginEmail.jsx";
-import LoginPassword from "./pages/loginPassword.jsx";
+import LoginEmail from "./LoginPages/loginEmail.jsx";
+import LoginPassword from "./LoginPages/loginPassword.jsx";
 import ReportPage from "./pages/ReportPage.jsx"; // Importing Report Page
-import SignUpConfirmation from "./pages/signUpConfirmation.jsx";
-import SignUpEmail from "./pages/signUpEmail.jsx";
-import SignUpPassword from "./pages/signUpPassword.jsx";
-import SignUpUsername from "./pages/signUpUsername.jsx";
+import SignUpConfirmation from "./SignUpPages/signUpConfirmation.jsx";
+import SignUpEmail from "./SignUpPages/signUpEmail.jsx";
+import SignUpPassword from "./SignUpPages/signUpPassword.jsx";
+import SignUpUsername from "./SignUpPages/signUpUsername.jsx";
 import UserDashboard from "./pages/userDashboard.jsx";
 import AdminDashboard from "./SysAdminPages/adminDashboard.jsx";
 import ResetPassword from "./SysAdminPages/resetPassword.jsx";
@@ -21,13 +21,25 @@ function App() {
 		<React.StrictMode>
 			<Router>
 				<Routes>
-					<Route path="/" element={<LandingPage />} />{" "}
-					<Route path="/login/email" element={<LoginEmail />} />
-					{/* Route for login page */}
-					<Route path="/login/password" element={<LoginPassword />} />
-					<Route path="/signup/email" element={<SignUpEmail />} />
-					<Route path="/report" element={<ReportPage />} />{" "}
-					{/* New Report Page Route */}
+					{/* Starting Page */}
+					<Route 
+						path="/" 
+						element={<LandingPage />} 
+					/>
+					{/* Routes for login page */}
+					<Route 
+						path="/login/email" 
+						element={<LoginEmail />} 
+					/>
+					<Route 
+						path="/login/password" 
+						element={<LoginPassword />} 
+					/>
+					{/* Routes for signUp page */}
+					<Route 
+						path="/signup/email" 
+						element={<SignUpEmail />} 
+					/>
 					<Route
 						path="/signup/username"
 						element={<SignUpUsername />}
@@ -40,10 +52,25 @@ function App() {
 						path="/signup/confirmation"
 						element={<SignUpConfirmation />}
 					/>
-					<Route path="/detector" element={<Detector />} />
-					{/* Route for SysAdmin page */}
-					<Route path="/admin-login" element={<AdminLogin />} />
-					<Route path="/user-dashboard" element={<UserDashboard />} />
+					{/* Route for Report page */}
+					<Route 
+						path="/report" 
+						element={<ReportPage />} 
+					/>
+					{/* Route for detector page */}
+					<Route 
+						path="/detector" 
+						element={<Detector />} 
+					/>
+					{/* Routes for SysAdmin pages */}
+					<Route 
+						path="/admin-login" 
+						element={<AdminLogin />} 
+					/>
+					<Route 
+						path="/user-dashboard" 
+						element={<UserDashboard />} 
+					/>
 					<Route
 						path="/admin/dashboard"
 						element={<AdminDashboard />}
@@ -52,7 +79,10 @@ function App() {
 						path="/admin/dashboard/:userId"
 						element={<UserDetails />}
 					/>
-					<Route path="/reset-password" element={<ResetPassword />} />
+					<Route 
+						path="/reset-password" 
+						element={<ResetPassword />} 
+					/>
 				</Routes>
 			</Router>
 		</React.StrictMode>
