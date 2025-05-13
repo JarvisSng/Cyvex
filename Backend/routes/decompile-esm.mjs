588b9d05525f99d3aa2204ad1029fd6937a5f7bc
JarvisSng <jarvissng@gmail.com>
2025-05-13 11:37:06 +0800

    Removed broken function

--- Backend/routes/decompile-esm.mjs
+++ Backend/routes/decompile-esm.mjs
@@ -2,7 +2,7 @@
 import { Contract } from 'sevm';
 
 export async function decompileBytecode(bytecode) {
-  const contract = new Contract(bytecode).patchdb();
+  const contract = new Contract(bytecode)
   const pseudocode = contract.solidify();
   return pseudocode;
 }
