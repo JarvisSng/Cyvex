e4997af7e00fb45263813881f7dc35760c6354b9
JarvisSng <150510665+JarvisSng@users.noreply.github.com>
2025-05-14 00:38:23 +0800

    Fixes

--- Backend/routes/evmRoutes.js
+++ Backend/routes/evmRoutes.js
@@ -11,7 +11,7 @@ router.get("/evm-opcodes", async (req, res) => {
 	try {
 		const { data, error } = await supabase
 			.from("evm_opcodes")
-			.select("opcode, mnemonic, solidity_function");
+			.select("opcode, mnemonic");
 		if (error) {
 			console.error("Error fetching EVM opcodes:", error);
 			return res.status(400).json({ error: error.message });
