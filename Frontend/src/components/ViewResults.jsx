27dde0cf6bb5d9b9d48245ce499217026bd4ec11
JarvisSng <150510665+JarvisSng@users.noreply.github.com>
2025-05-17 16:07:24 +0800

    One line fix

--- Frontend/src/components/ViewResults.jsx
+++ Frontend/src/components/ViewResults.jsx
@@ -176,7 +176,8 @@ export default function ViewResults({code, fileExt}) {
     const extensionMap = {
       py: 'Python',
       js: 'JavaScript',
-      java: 'Java'
+      java: 'Java',
+      cpp : 'C++'
     };
 
     if (fileExt) {
